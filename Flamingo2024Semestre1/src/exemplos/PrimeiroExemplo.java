e5efe72079d4936e6ae75093e93babf687b6d324
Esther <125416224+estheralv@users.noreply.github.com>
2024-03-20 14:26:38 -0300

    Delete Flamingo2024Semestre1 directory

--- Flamingo2024Semestre1/src/exemplos/PrimeiroExemplo.java
+++
@@ -1,8 +0,0 @@
-package exemplos;
-
-public class PrimeiroExemplo {
-	public static void main(String[] args) {
-		System.out.println("valha");
-		System.out.print("osh");
-	}
-}
--- Flamingo2024Semestre1/src/exemplos/SegundoExemplo.java
+++
@@ -1,9 +0,0 @@
-package exemplos;
-
-public class SegundoExemplo {
-	public static void main(String[] args) {
-		System.out.println("Esther");
-		System.out.println("18");
-		System.out.print("Rua Yolanda Mahali");
-	}
-}
