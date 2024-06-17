9ec122996faa22e241ff3a10c0a0bb52cafcf241
Esther <125416224+estheralv@users.noreply.github.com>
2024-06-17 20:43:05 -0300

    Add files via upload

---
+++ bduser_register.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE userdb;
+USE userdb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL
+);
